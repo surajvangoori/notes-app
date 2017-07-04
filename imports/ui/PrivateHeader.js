08a3c1edb616729d081456027ae99c26d58eb574
Suraj Vangoori <suraj@adventurify.com>
2017-07-04 09:15:27 -0500

    Added Nav styles for mobile screens

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -5,12 +5,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import {Accounts} from 'meteor/accounts-base';
+import {Session} from 'meteor/session';
 
 //Had to declare a variable because using the propTypes below
 export const PrivateHeader = (props)=>{
+    const navImageSrc = props.isNavOpen ? '/images/x.svg':'/images/bars.svg';
+
     return(
             <div className="header">
               <div className="header__content">
+                <img className="header__nav-toggle" onClick={props.handleNavToggle} src={navImageSrc}/>
                 <h1 className="header__title">{props.title}</h1>
                 <button className="button button--link-text" onClick={()=> props.handleLogout()}>Logout</button>
               </div>
@@ -20,13 +24,17 @@ export const PrivateHeader = (props)=>{
 
 PrivateHeader.propTypes={
     title:PropTypes.string.isRequired,
-    handleLogout:PropTypes.func.isRequired
+    handleLogout:PropTypes.func.isRequired,
+    isNavOpen:PropTypes.bool.isRequired,
+    handleNavToggle:PropTypes.func.isRequired
 };
 
 export default createContainer(()=>{
     return{
-        handleLogout:()=>{
-            Accounts.logout();
-        }
+        handleLogout:()=>{Accounts.logout()},
+        handleNavToggle: ()=>{
+            Session.set('isNavOpen',!(Session.get('isNavOpen')));
+        },
+        isNavOpen: Session.get('isNavOpen')
     }
 }, PrivateHeader);
